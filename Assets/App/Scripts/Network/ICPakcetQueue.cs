580ad1cd04bcd5776b1489becf27b5217af49528
YU <82423176+tmddn0230@users.noreply.github.com>
2024-07-02 21:53:37 +0900

    comom
    ss

---
+++ Assets/App/Scripts/Network/ICPakcetQueue.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Not Use MonoBehaviour
+// This Class Only Pull, Push Packet
+public class ICPakcetQueue 
+{
+    public static ICPakcetQueue Instance { get; } = new ICPakcetQueue();
+                 //Queue<IPacket> _packetQueue - new Queue<IPacket>();
+
+
+
+
+}

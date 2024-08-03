10e3f49afa76c97067efd33dc582cc2976c7889b
cksdud5549 <68266702+cksdud5549@users.noreply.github.com>
2024-08-04 01:43:40 +0900

    음성채팅 c#서버 초기 단순 udp통신 형태만
    음성채팅 c#서버 초기 단순 udp통신 형태만

--- Server/ICVoiceServer/UDPServer.h
+++ Server/ICVoiceServer/UDPServer.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+
 class UDPServer
 {
-};
+public:
+    UDPServer();
+    ~UDPServer();
+    bool Initialize();
+    void StartListening();
+    void Cleanup();
 
+private:
+    SOCKET ListenSocket;
+    struct addrinfo* result;
+    struct addrinfo hints;
+};

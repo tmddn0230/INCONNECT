388d3642138a0df96e5a02dd76d81fa6b76f9da1
KyungunJung <10509060@naver.com>
2024-08-06 21:35:03 +0900

    0806
    ㅁ

--- Assets/App/Scripts/Client/ICClient.cs
+++ Assets/App/Scripts/Client/ICClient.cs
@@ -6,11 +6,19 @@ public class ICClient : MonoBehaviour
 {
     // TCP Connect 시 서버로 부터 받음
     int UID;
-    // UID : 0 이면 male , 1 이면 female
+    // UID : 1 이면 male , 2 이면 female
     bool IsMale;
 
 
-    
+
+    public void Actor_Spawn(int uid, bool Ismale)
+    {
+        // uid 1이면 male 소환하고,
+        // 각 프리팹에서 getcomponent<ICInputManager>()을 하고 
+        //   
+        // 
+    }
+
     // Start is called before the first frame update
     void Start()
     {

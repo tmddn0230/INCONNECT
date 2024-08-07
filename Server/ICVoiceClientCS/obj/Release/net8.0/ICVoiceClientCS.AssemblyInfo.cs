250939109ba8d31735d75981d0c9c5690e884ecc
cksdud5549 <68266702+cksdud5549@users.noreply.github.com>
2024-08-07 19:02:25 +0900

    음챗0807

--- Server/ICVoiceClientCS/obj/Release/net8.0/ICVoiceClientCS.AssemblyInfo.cs
+++ Server/ICVoiceClientCS/obj/Release/net8.0/ICVoiceClientCS.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ICVoiceServerCS")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ICVoiceClientCS")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776b254625687fa2840d43523351306773253779")]
-[assembly: System.Reflection.AssemblyProductAttribute("ICVoiceServerCS")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ICVoiceServerCS")]
+[assembly: System.Reflection.AssemblyProductAttribute("ICVoiceClientCS")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ICVoiceClientCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
